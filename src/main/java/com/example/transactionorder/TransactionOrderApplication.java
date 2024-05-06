d28ec05788d31d9bc6247675939ec446fb00e34f
Magdalenacze <126948179+Magdalenacze@users.noreply.github.com>
2024-05-06 17:44:09 +0200

    Task 1 - Create a project and publish it to your repository

---
+++ src/main/java/com/example/transactionorder/TransactionOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/transactionorder/TransactionOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transactionorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
