40ec6328ed01b27acdb34460842c59bd1be2e5cc
Magdalenacze <126948179+Magdalenacze@users.noreply.github.com>
2024-05-07 13:16:03 +0200

    Story 3 - commit 1
    Adding products has been implemented

--- src/main/java/com/example/transactionorder/entity/ProductEntity.java
+++ src/main/java/com/example/transactionorder/entity/ProductEntity.java
@@ -6,13 +6,16 @@ import lombok.*;
 @Entity
 @Table(name = "products")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class ProductEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "name")
     private String name;
+
     private Integer quantity;
 
     public ProductEntity(String name, Integer quantity) {
--- src/main/java/com/example/transactionorder/service/ProductServiceImpl.java
+++ src/main/java/com/example/transactionorder/service/ProductServiceImpl.java
@@ -4,16 +4,27 @@ import com.example.transactionorder.dto.ProductDto;
 import com.example.transactionorder.entity.ProductEntity;
 import com.example.transactionorder.repository.ProductRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
-@org.springframework.stereotype.Service
+import java.util.List;
+
+@Service
 @AllArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
-    private final ProductRepository transactionOrderRepository;
+    private final ProductRepository productRepository;
 
     @Override
     public void addProduct(ProductDto productDto) {
         ProductEntity productEntity = new ProductEntity(productDto.getName(), productDto.getQuantity());
-        transactionOrderRepository.save(productEntity);
+        productRepository.save(productEntity);
+    }
+
+    @Override
+    public List<ProductDto> getProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(e -> new ProductDto(e.getName(), e.getQuantity()))
+                .toList();
     }
 }
